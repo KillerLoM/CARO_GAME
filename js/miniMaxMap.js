938a08998c0f4e1e7293cff815e7bdeaba8b46e2
KillerLoM <nguyenminhtam01632.fetel@gmail.com>
2023-09-15 16:23:24 +0700

    Update js/caro.js

--- js/miniMaxMap.js
+++ js/miniMaxMap.js
@@ -1,17 +1,17 @@
 const MAP_SCORE_BOT_HARD = new Map([
   [5, 11],
-  [4, 7],
-  [3, 5],
-  [2, 4],
-  [1, 2],
+  [4, 8],
+  [3, 6],
+  [2, 5],
+  [1, 3],
   [0, 1]
 ]);
 const MAP_SCORE_USER_HARD = new Map([
   [5, 10],
   [4, 9],
-  [3, 8],
-  [2, 6],
-  [1, 3],
+  [3, 7],
+  [2, 4],
+  [1, 2],
   [0, 0],
 ]);
 const MAP_SCORE_BOT_EASY = new Map([
